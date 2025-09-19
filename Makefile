5a78e8d89e5ee91f8cc68d870ed5b26704c6b20b
inaki <iecosta-estra@hotmail.com>
2025-09-18 23:41:30 -0300

    hasta ej 7 inclusive

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ all: practica01
 practica01: Practica01.hs
 	ghc Practica01.hs -o practica01
 
-run:
+run_practica01:
 	$(MAKE) practica01
 	./practica01
 	$(MAKE) clean
--- Practica01.hs
+++ Practica01.hs
@@ -1,3 +1,4 @@
+import Control.Monad.Accum (MonadAccum(accum))
 -- Ejercicio 01
 max2:: (Float, Float) -> Float
 max2 (x, y)
@@ -63,6 +64,24 @@ elemFoldr e = foldr (\x ac -> x == e || ac) False
 concatFoldr :: [a] -> [a] -> [a]
 concatFoldr xs ys = foldr (\y ac -> y : ac) ys xs
 
+filterFoldr :: Eq a => a -> [a] -> [a]
+filterFoldr e = foldr (\x ac -> if x == e then x : ac else ac) []
+
+mapFoldr :: (a -> b) -> [a] -> [b]
+mapFoldr f = foldr (\x ac -> f x : ac) []
+
+mejorSegun :: (a -> a -> Bool) -> [a] -> a
+mejorSegun f [] = error "lista vacia"
+mejorSegun f (x:xs) = foldr (\x ac -> if f x ac then x else ac) x xs
+
+sumasParciales:: Num a => [a] -> [a]
+sumasParciales xs = [ sum (take i xs) | i <- [1..length xs]]
+
+sumaAlt:: Num a => [a] -> a
+sumaAlt xs = snd (foldr f (if even (length xs) then (-1, 0) else (1, 0)) xs)
+    where
+        f x (signo, res) = (-signo, res + signo * x)
+
 ejercicio03 :: IO ()
 ejercicio03 = do
     print "sumFoldr [1, 2, 3]"
@@ -80,9 +99,140 @@ ejercicio03 = do
     print "concatFoldr [1, 2, 3] [4, 5]"
     print (concatFoldr [1, 2, 3] [4, 5])
 
+    print "filterFoldr 2 [1, 2, 3, 4, 2]"
+    print (filterFoldr 2 [1, 2, 3, 4, 2])
+
+    print "filterFoldr 2 [1, 3, 4]"
+    print (filterFoldr 2 [1, 3, 4])
+
+    print "mapFoldr (*2) [1, 2, 3]"
+    print (mapFoldr (*2) [1, 2, 3])
+
+    print "mejorSegun (<) [1, 2, 3, -1]"
+    print (mejorSegun (<) [1, 2, 3, -1])
+
+    print "mejorSegun (>) [1, 2, 3, -1]"
+    print (mejorSegun (>) [1, 2, 3, -1])
+
+    print "sumasParciales [1, 2, 3, -1]"
+    print (sumasParciales [1, 2, 3, -1])
+
+    print "sumaAlt [1, 2, 3, -1]"
+    print (sumaAlt [1, 2, 3, -1])
+
+    print "sumaAlt [1, 2, 3]"
+    print (sumaAlt [1, 2, 3])
+
+
+ 
+{- 
+Ejercicio05
+Considerar las funciones
+
+elementosEnPosicionesPares :: [a] -> [a]
+elementosEnPosicionesPares [] = []
+elementosEnPosicionesPares (x:xs) = 
+    if null xs then [x]
+    else x : elementosEnPosicionesPares tail(xs)
+
+entrelazar :: [a] -> [a] -> [a]
+entrelazar [] = id
+entrelazar (x:xs) = \ys -> 
+    if null ys then x : entrelazar xs []
+    else x : head ys : entrelazar xs (tail ys)
+-}
+
+{- 
+elementosEnPosicionesPares NO es una induccion estrutural, porque modifica xs al hacer la recursión. En concreto, hace tail xs para saltearse valores. Foldr va procesando cada elemento en orden
+-}
+{- 
+entrelazar TAMPOCO es una induccion estrutural, porque modifica xs e ys al hacer la recursión.
+-}
+
+recr :: (a->[a]->b->b) -> b -> [a] -> b
+recr f z [] = z
+recr f z (x:xs) = f x xs (recr f z xs)
+
+sacarUna :: Eq a => a -> [a] -> [a]
+sacarUna e = recr (\x xs r -> if x == e then xs else x:r) []
+
+insertarOrdenado :: Ord a => a -> [a] -> [a]
+insertarOrdenado e = recr (\x xs r -> if null xs then if x > e then (e:[x]) else (x:[e]) else if x > e then (e:x:xs) else x:r) []
+
+ejercicio06 :: IO()
+ejercicio06 = do
+    print "sacarUna 2 [1, 2, 3]"
+    print (sacarUna 2 [1, 2, 3])
+
+    print "sacarUna 4 [1, 2, 3]"
+    print (sacarUna 4 [1, 2, 3])
+
+    print "insertarOrdenado 3 [1, 2, 4]"
+    print (insertarOrdenado 3 [1, 2, 4])
+
+    print "insertarOrdenado 4 [1, 2, 3]"
+    print (insertarOrdenado 4 [1, 2, 3])
+
+{-
+Ejercicio 07
+-}
+
+mapPares :: ((a, b) -> c) -> [(a, b)] -> [c]
+mapPares f = recr (\x xs r -> f x : r) []
+
+
+armarPares :: [a] -> [b] -> [(a, b)]
+armarPares xs ys = recr zipWith' [] xs
+    where
+        zipWith' x xs r = case ys of
+            [] -> []
+            (y:ys') -> (x, y) : armarPares xs ys'
+
+-- | null xs = [(x, head y)]
+
+mapDoble::((a, b) -> c) -> [a] -> [b] -> [c]
+mapDoble f xs ys = recr zipWith' [] xs
+    where
+        zipWith' x xs r = case ys of
+            [] -> []
+            (y:ys') -> f (x, y) : mapDoble f xs ys'
+
+mapDoble2::((a, b) -> c) -> [a] -> [b] -> [c]
+mapDoble2 f xs ys = mapPares f (armarPares xs ys)
+
+ejercicio07 :: IO()
+ejercicio07 = do
+    print "mapPares sumaTupla [(1, 2), (3, 4), (5, 6)]"
+    print (mapPares sumaTupla [(1, 2), (3, 4), (5, 6)])
+
+    print "armarPares [1, 2, 3] [1, 2, 3]"
+    print (armarPares [1, 2, 3] [1, 2, 3])
+
+    print "armarPares [1, 2, 3, 4] [1, 2, 3]"
+    print (armarPares [1, 2, 3, 4] [1, 2, 3])
+
+    print "mapDoble sumaTupla [1, 2, 3] [1, 2, 3]"
+    print (mapDoble sumaTupla [1, 2, 3] [1, 2, 3])
+
+    print "mapDoble sumaTupla [1, 2, 3, 4] [1, 2, 3]"
+    print (mapDoble sumaTupla [1, 2, 3, 4] [1, 2, 3])
+
+    print "mapDoble2 sumaTupla [1, 2, 3, 4] [1, 2, 3]"
+    print (mapDoble2 sumaTupla [1, 2, 3, 4] [1, 2, 3])
+    
+{-
+Ejercicio 09
+-}
+ejercicio09 :: IO()
+ejercicio09 = do
+    print "FoldNat"
+
 -- Ejemplo de uso e impresión en consola
 main :: IO ()
 main = do
-    ejercicio01
-    ejercicio02
-    ejercicio03
+    --ejercicio01
+    --ejercicio02
+    --ejercicio03
+    --ejercicio06
+    --ejercicio07
+    ejercicio09
